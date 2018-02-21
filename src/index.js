ce5b69685b51dbd51014934fc3591acd5135ac3f
Klachkevich Dmitry <klachkevich.dmitry@gmail.com>
2018-02-21 10:23:01 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var base=equation.split(" ");
+    base[1].split("x");
+
+    var a=Number(base[0]);
+    var b=Number(base[3]+base[4]);
+    var c=Number(base[7]+base[8]);
+
+    var d=b*b-4*a*c;
+
+    var firstSolution=((-b+Math.sqrt(d))/(2*a));
+    var secondSolution=((-b-Math.sqrt(d))/(2*a));
+
+    var solution=[Math.round(firstSolution), Math.round(secondSolution)];
+    return solution.sort((a,b)=>a-b);
 }
